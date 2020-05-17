1095d888a8a754152deae742fe735c5319935935
ABHINAV P <58884437+Codefor-Future@users.noreply.github.com>
2020-05-17 11:10:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,30 +2,14 @@ let app = require("express")();
 let http= require("http").Server(app)
 let io= require("socket.io")(http)
 let bodyParser= require('body-parser')
-var users= [
-    {}
-]
+
 app.use(bodyParser.json());
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/index.html")
 });
 
-app.post("/login", (req,res)=>{
-    console.log(req.body)
-    switch(req.body.username ){
-        case "abhinav":{
-            if(req.body.password==="1190"){
-                res.send({
-                    "loggedIn":true,
-                    "user":"abhi"
-                })
-            }
-        }
-        
-    }
-        
-})
+
 
 io.on("connection", (socket)=>{
     console.log("One connected");
